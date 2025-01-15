dcd1037327ceda1bd69fcd64b2f9ff73a445aea2
Deepesh Prajapati <prajapatideepesh055@gmail.com>
2025-01-15 16:10:34 +0530

    changes

--- src/main/java/com/todo/model/Todo.java
+++ src/main/java/com/todo/model/Todo.java
@@ -4,12 +4,15 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Todo {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 	private LocalDate date;
